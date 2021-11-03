7561fc3e64739350ed45fbf26f700a22b5b939cd
cPicton <pictonchris2@gmail.com>
2021-11-03 11:53:10 +0300

    initial commit

---
+++ pass.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point.
+ * Description: code that passes Betty checks
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("Hello beautiful Betty, check me out!\n");
+	return (0);
+}
